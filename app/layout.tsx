16ae253517042d818593fc7c268ec133678bff5f
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2025-02-23 22:46:14 -0300

    Refactor page component to update links

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 16 16%22><text y=%2214%22 font-size=%2214%22>🦋</text></svg>" />
+      </head>
       <body>{children}</body>
     </html>
   )
--- page.tsx
+++ page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <h1 className="text-white text-2xl md:text-4xl font-bold text-center px-4 flex flex-col gap-2">
           <div>Qué perdiste</div>
         </h1>
-        <br></br>
+        <div>
         <Link
           href="https://www.instagram.com/eugeniosaintemarie"
           target="_blank"
@@ -18,11 +18,12 @@ export default function Page() {
           <Instagram className="w-4 h-4" />
           <span className="sr-only">Instagram</span>
         </Link>
+        </div>
       </main>
 
       <footer className="p-4 flex justify-center">
         <Link
-          href="https://www.eugeniosaintemrie.github.io"
+          href="https://www.eugeniosaintemarie.github.io"
           rel="noopener noreferrer"
           className="text-white hover:text-gray-300 transition-colors"
         >
